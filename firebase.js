1fd491209ccdf7b9eec19b3adf830f93934d8b77
clintonsinkari18 <clintonsinkari18@gmail.com>
2026-04-20 10:41:27 +0000

    connected again

--- firebase.js
+++ firebase.js
@@ -1,41 +1,16 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDJCTKw8UAq4lZwI5PucDIB84PlH77GzaA",
-    authDomain: "fir-voting-21661.firebaseapp.com",
-    projectId: "fir-voting-21661",
-    storageBucket: "fir-voting-21661.firebasestorage.app",
-    messagingSenderId: "447540369173",
-    appId: "1:447540369173:web:e31f6ad87e4a085b547b1c",
-    measurementId: "G-L422EWKQ4X"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add your own Firebase configuration from your Firebase project settings
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDJCTKw8UAq4lZwI5PucDIB84PlH77GzaA",
+  authDomain: "fir-voting-21661.firebaseapp.com",
+  projectId: "fir-voting-21661",
+  storageBucket: "fir-voting-21661.firebasestorage.app",
+  messagingSenderId: "447540369173",
+  appId: "1:447540369173:web:e31f6ad87e4a085b547b1c",
+  measurementId: "G-L422EWKQ4X"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Get a Firestore instance
 export const db = getFirestore(app);
